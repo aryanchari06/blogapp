e682f556cbb255c34af4805142a9cc32197c033d
aryanchari06 <aryaney0704@gmail.com>
2024-08-06 11:24:41 +0530

    database, fileupload and queries

---
+++ src/appwrite/config.js
@@ -0,0 +1,125 @@
+import { Databases, Query } from "appwrite";
+import conf from "../conf/conf";
+
+export class Service {
+    client = new Client()
+    databases
+    storage
+
+    constructor() {
+        this.client.setEndpoint(conf.appwriteUrl)
+            .setProject(conf.projectId)
+
+        this.databases = new Databases(this.client)
+        this.storage = new Storage(this.client)
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(conf.databaseId, conf.collectionId, slug, {
+                title,
+                content,
+                featuredImage,
+                status,
+                userId
+            })
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.databaseId,
+                conf.collectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                })
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async deleteDocument(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.databaseId,
+                conf.collectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            console.log("Error: ", error)
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.databaseId,
+                conf.collectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Error at getPost(): ", error)
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal('status', 'active')]) {
+        try {
+            return await this.databases.listDocuments(
+                conf.databaseId,
+                conf.collectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("Error at getPosts(): ", error)
+            return false
+        }
+    }
+
+    //file services
+
+    async uploadFile(file) {
+        try {
+            return await this.storage.createFile(
+                conf.bucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Error at uploadFile(): ", error)
+            return false
+        }
+    }
+
+    async deleteFile(fileID) {
+        try {
+            await this.storage.deleteFile(
+                conf.bucketId,
+                fileID
+            )
+            return true
+        } catch (error) {
+            console.log("Error at deleteFile(): ", error)
+            return false
+        }
+    }
+
+    getFilePreview(fileID) {
+        this.storage.getFilePreview(
+            conf.bucketId,
+            fileID
+        )
+    }
+}
+
+const service = new Service
+export default service
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,7 +1,7 @@
 const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     projectId : String(import.meta.env.VITE_PROJECT_ID),
-    batabaseId : String(import.meta.env.VITE_DATABSE_ID),
+    databaseId : String(import.meta.env.VITE_DATABSE_ID),
     collectionId : String(import.meta.env.VITE_COLLECTION_ID),
     bucketId : String(import.meta.env.VITE_BUCKET_ID),
 }
