b770403bed5bdbf34b9fef556c991eb6bf596db1
aryanchari06 <aryaney0704@gmail.com>
2024-08-06 19:39:09 +0530

    added other components

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Button({
+    children,
+    type = 'button',
+    bgColor = 'bg-blue-600',
+    textColor = 'white',
+    className = '',
+    ...props
+}) {
+    return (
+        <button className={`${className} ${textColor} ${bgColor}`} {...props}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./header/Header";
 import Footer from './footer/Footer'
+import Container from "postcss";
+import Logo from "./Logo";
+import LogOutBtn from './footer/LogOutBtn'
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogOutBtn
 }
